de75975dbd634101210a18a78f3f5bd9cdeabffe
alek <alek@alek.com>
2022-12-01 18:35:37 +0100

    add python script to convert problem descriptions to markdown, can take input from a file or a pipe, ex. curl -s https://adventofcode.com/2022/day/1 | ./convert.py > desc.md

---
+++ convert.py
@@ -0,0 +1,30 @@
+#!/usr/bin/env python3
+
+from sys import argv, stderr, stdin
+
+from bs4 import BeautifulSoup as bs
+from markdownify import MarkdownConverter
+
+if __name__ == '__main__':
+
+    html_data = None
+    if len(argv) > 1:
+        with open(argv[1]) as f:
+            html_data = f.read()
+    else:
+        html_data = stdin.read()
+
+    input_file = argv[1] if len(argv) > 1 else stdin
+
+    parsed = bs(html_data, 'html.parser')
+    article = parsed.find('article')
+
+    opts = {
+            'wrap': True,
+            'wrap_width': 80,
+            'bullets': '+',
+            }
+
+    md_data = MarkdownConverter(**opts).convert_soup(article)
+
+    print(md_data, end='')
