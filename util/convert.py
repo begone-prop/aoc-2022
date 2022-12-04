c357fa027301a30997a0d08957a63de7f3106e8c
alek <alek@alek.com>
2022-12-04 18:18:32 +0100

    update convert.py to be able to download full problem descriptions, including for part 2

--- util/convert.py
+++ util/convert.py
@@ -1,21 +1,38 @@
 #!/usr/bin/env python3
 
 from sys import argv, stderr, stdin
+from os import getenv
+from requests import get
 
 from bs4 import BeautifulSoup as bs
 from markdownify import MarkdownConverter
 
 if __name__ == '__main__':
 
-    html_data = None
-    if len(argv) > 1:
-        with open(argv[1]) as f:
-            html_data = f.read()
-    else:
-        html_data = stdin.read()
+    base_url = 'https://adventofcode.com/2022'
 
-    parsed = bs(html_data, 'html.parser')
-    article = parsed.find('article')
+    if len(argv) == 1:
+        print('No argument', file=stderr)
+        exit(1)
+
+    day = argv[1]
+
+    cookie = getenv('AOC_COOKIE')
+    if not cookie or not len(cookie):
+        print("AOC_COOKIE not found or empty", file=stderr)
+        exit(1)
+
+    url = f'{base_url}/day/{day}'
+
+    head = { 'Cookie': f'session={cookie}' }
+
+    resp = get(url, headers=head)
+    if resp.status_code != 200:
+        print(f'Error fetching description from {url}: Server returned {resp.status_code}')
+        exit(1)
+
+    parsed = bs(resp.content, 'html.parser')
+    article = parsed.find_all('article')
 
     opts = {
             'wrap': True,
@@ -23,6 +40,6 @@ if __name__ == '__main__':
             'bullets': '+',
             }
 
-    md_data = MarkdownConverter(**opts).convert_soup(article)
-
-    print(md_data, end='')
+    for art in article:
+        md_data = MarkdownConverter(**opts).convert_soup(art)
+        print(md_data, end='')
